1adcf06eed46a412b66b6c914d4cca8b07ad76c1
Immanuvel15 <immanuvelrichards@gmail.com>
2025-07-08 21:36:45 +0600

    Update signals.py

--- backend_project/study_material_hub/signals.py
+++ backend_project/study_material_hub/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from .models import UploaderProfile
+# save and exit 
 
 @receiver(post_save, sender=AbstractUser)
 def create_user_profile(sender, instance, created, **kwargs):
